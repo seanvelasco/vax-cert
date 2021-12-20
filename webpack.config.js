2c92ac136f77ac00abf6791d04d9d5e961e3e90e
Sean Velasco <seanvelasco@ieee.org>
2021-12-20 16:43:15 +0800

    Enabled copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
@@ -32,7 +34,12 @@ module.exports = {
 		new HtmlWebpackPlugin({
 		template: path.resolve( __dirname, 'public/index.html' ),
 		filename: 'index.html'
-	})
+	}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: 'public' }
+			]
+		})
 	],
 	devServer: {
 		port: 4000
