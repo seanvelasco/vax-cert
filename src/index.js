788f516546f1afe86a4ead56d095bf0ec9fd5c6e
Sean Velasco <seanvelasco@ieee.org>
2021-12-20 16:32:07 +0800

    Loaded styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import GlobalStyles from './GlobalStyles'
+import './index.css'
 
 ReactDOM.render(
     <React.StrictMode>
